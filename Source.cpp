fd60699c8b28a637fd6498c3d865f2230ae46095
AlexanderDingus <91158187+AlexanderDingus@users.noreply.github.com>
2021-09-21 16:46:00 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,10 @@
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
+#include <conio.h>
 using namespace std;
 
+//This should print out the level of every character
 void printLevels(int levels[], string letters[]){
     for (int i = 0; i < 26; i++) {
         cout << "The " << letters[i] << " is level " << levels[i] << endl;
@@ -11,6 +13,7 @@ void printLevels(int levels[], string letters[]){
     return;
 }
 
+//This should level up one random character
 void lootbox(int levels[], string letters[]) {
     int loot = rand() % 26;
     cout << "The '" << letters[loot] << "' went from " << levels[loot] << " to " << levels[loot] + 1 << "!" << endl;
@@ -18,6 +21,17 @@ void lootbox(int levels[], string letters[]) {
     return;
 }
 
+int total(int levels[]) {
+    int owned = 0;
+    for (int i = 0; i < 26; i++) {
+        if (levels[i] > 0) {
+            owned++;
+        }
+    }
+    return owned;
+}
+
+//
 int main()
 {
     cout << "Welcome to..." << endl;
@@ -30,23 +44,31 @@ int main()
     char userI;
     int sentinelValue = 0;
     while (sentinelValue != 1) {
+
+        system("CLS");
         cout << endl;
+        cout << "You own " << total(levels) << " Alphabet Warriors" << endl;
         cout << "What do you want to do?" << endl;
         cout << "Type 0 to quit" << endl;
         cout << "Type 1 to check your Alphabet Army's levels" << endl;
         cout << "Type 2 to open a lootbox" << endl;
-        cin >> userInput;
-        userI = userInput[0];
+        //cin >> userInput;
+        //userI = userInput[0];
+        userI = _getch();
+        cout << endl;
         switch(userI){
             case '0':
                 cout << "Quitting !" << endl;
+                system("pause");
                 sentinelValue = 1;
                 break;
             case '1':
                 printLevels(levels, letters);
+                system("pause");
                 break;
             case '2':
                 lootbox(levels, letters);
+                system("pause");
                 break;
         }
     }
